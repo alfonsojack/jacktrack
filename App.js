09104da17210eb08201ccd36cfb29ce72bf83678
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-13 17:03:59 -0700

    Play with text

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome To</Text>
+      <Text>Jack Track</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fdfa',
     alignItems: 'center',
     justifyContent: 'center',
   },
