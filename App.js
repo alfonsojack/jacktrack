f2bbb6c04f91b72aae1df2311732c84ada4e2c23
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-13 17:20:00 -0700

    Add Navigation container

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome To</Text>
-      <Text>Jack Track</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Welcome To</Text>
+        <Text>Jack Track</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
